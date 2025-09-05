ee7f06edcead94ab5e6a96bdb4fe267f67af284a
atsushi-tmdu <atsenda@hotmail.com>
2025-09-05 17:21:03 +0900

    rechanged next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
 };
-module.exports = nextConfig;
-
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
 export default nextConfig;
